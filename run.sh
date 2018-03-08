a6b33e8dce8c9dadee956f0fa39876bced7857b0
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-03-08 16:25:03 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,25 +23,11 @@ perl -p -i -e 's/\(`correlation_id`\)/(`correlation_id`(255))/' /opt/homer_mysql
 
 # Reconfigure rotation
 
-# --- I believe these are deprecated (@dougbtv, 3/30/17)
-# export PATH_ROTATION_SCRIPT=/opt/homer_rotate
-# chmod 775 $PATH_ROTATION_SCRIPT
-# chmod +x $PATH_ROTATION_SCRIPT
-
 export PATH_ROTATION_CONFIG=/opt/rotation.ini
 
 perl -p -i -e "s/homer_user/$DB_USER/" $PATH_ROTATION_CONFIG
 perl -p -i -e "s/homer_password/$DB_PASS/" $PATH_ROTATION_CONFIG
 perl -p -i -e "s/localhost/$DB_HOST/" $PATH_ROTATION_CONFIG
-
-# --- I believe these are deprecated (@dougbtv, 3/30/17)
-# PERL_SCRIPTS=(/opt/homer_mysql_new_table.pl /opt/homer_mysql_partrotate_unixtimestamp.pl)
-# for perl_script in ${PERL_SCRIPTS[@]}
-# do
-#   perl -p -i -e "s/homer_user/$DB_USER/" $perl_script
-#   perl -p -i -e "s/homer_password/$DB_PASS/" $perl_script
-#   perl -p -i -e "s/mysql_host = \"localhost\"/mysql_host = \"$DB_HOST\"/" $perl_script
-# done
 
 # Init rotation
 /opt/homer_mysql_rotate
@@ -49,11 +35,11 @@ perl -p -i -e "s/localhost/$DB_HOST/" $PATH_ROTATION_CONFIG
 # Sleep Function
 sleep_until ()
 {
-  seconds=$(( $(date -d "$*" +%s) - $(date +%s) )) # Use $* to eliminate need for quotes
+  seconds=$(( $(date +%s) - $(date -d "$*" +%s) )) # calculate seconds distance to next rotation
   echo "Sleeping for $seconds seconds"
   sleep $seconds
 }
 
 # Run forever using $difference interval
 ROTATION_TIME="${ROTATION_TIME:-04:00}"
-bash -c "while true; do /opt/homer_mysql_rotate; set -e; sleep_until $ROTATION_TIME; set +e; done"   
+while true; do /opt/homer_mysql_rotate; set -e; sleep_until $ROTATION_TIME; set +e; done 
